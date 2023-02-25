28026d595cae8a46a1da4b970e216c35d3a808ca
juninkorea95 <juninkorea95@gmail.com>
2023-02-25 16:39:05 +0900

    feat

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,6 +7,8 @@ import Button from "../element/Button";
 import { modalVariants } from "../utils/animations/variants";
 import { isModalState } from "../utils/recoil/atoms";
 import { boxBorderRadius, flexCenter } from "../utils/styles/mixins";
+import { addNewUser } from "../utils/axios/axios";
+import { useMutation } from "react-query";
 
 const ModalWrapper = styled.section`
   position: absolute;
@@ -83,8 +85,26 @@ const Login = () => {
     handleSubmit,
     reset,
   } = useForm();
-  const onSubmit = (data) => {
 
+  // const {isLoading, isError, data} = useQuery("newUser", addNewUser)
+
+  const registerMutation = useMutation((newUser) => addNewUser(newUser))
+  
+
+  const onSubmit = async (data) => {
+  
+    if (isRegister !== true) {
+      
+    } else {
+      const newUser = {
+        username : data.userId,
+        email: data.email,
+        password: data.password
+      }
+      
+      const response = await registerMutation.mutateAsync(newUser)
+      console.log(response)
+    }
   };
 
   return (
