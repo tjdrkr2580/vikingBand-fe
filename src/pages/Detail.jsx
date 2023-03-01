7b71095cc933e1836a21ea935466001dff99aa06
tjdrkr2580 <tjdrkr2580@naver.com>
2023-03-01 21:40:37 +0900

    fix | memberName

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -3,7 +3,14 @@ import study from "../assets/study.jpg";
 import { useParams } from "react-router-dom";
 import { FaHeart } from "react-icons/fa";
 import { useMutation, useQuery, useQueryClient } from "react-query";
-import { deleteStudyRegist, getStudy, postStudyRegist, postStudyWish, postBoard, deleteBoard } from "../utils/axios/axios";
+import {
+  deleteStudyRegist,
+  getStudy,
+  postStudyRegist,
+  postStudyWish,
+  postBoard,
+  deleteBoard,
+} from "../utils/axios/axios";
 import { useRecoilValue } from "recoil";
 import { userInfoState } from "../utils/recoil/atoms";
 import { getUserDetailInfo } from "../utils/axios/axios";
@@ -20,11 +27,10 @@ import {
   StCommentText,
   StInput,
   BoardBox,
-  CommentInput
+  CommentInput,
 } from "./DetailStyle";
 
 const Detail = () => {
-
   const { id } = useParams();
 
   const queryClient = useQueryClient();
@@ -61,29 +67,39 @@ const Detail = () => {
   };
 
   // 스터디보드 제목/내용
-  const { value: title, onChange: titleChangeHandler, reset: resetTitle } = useInput("");
-  const { value: content, onChange: contentChangeHandler, reset: resetContent } = useInput("");
+  const {
+    value: title,
+    onChange: titleChangeHandler,
+    reset: resetTitle,
+  } = useInput("");
+  const {
+    value: content,
+    onChange: contentChangeHandler,
+    reset: resetContent,
+  } = useInput("");
 
-  
   //스터디보드 POST
   const board = {
     title,
-    content
-  }
+    content,
+  };
 
-  const postBoardMutate = useMutation(({id, board}) => postBoard({id, board}), {
-    onSuccess: () => {
-      queryClient.invalidateQueries("study")
+  const postBoardMutate = useMutation(
+    ({ id, board }) => postBoard({ id, board }),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("study");
+      },
     }
-  });
-  const onClickPostBoard = async ({id, board}) => {
-    if (board.title ==='' || board.content === '') {
-      alert ('제목과 내용을 입력해 주세요!')
+  );
+  const onClickPostBoard = async ({ id, board }) => {
+    if (board.title === "" || board.content === "") {
+      alert("제목과 내용을 입력해 주세요!");
     } else {
-      await postBoardMutate.mutateAsync({id, board});
+      await postBoardMutate.mutateAsync({ id, board });
     }
-    resetTitle()
-    resetContent()
+    resetTitle();
+    resetContent();
   };
 
   //스터디 보드 DELETE
@@ -93,16 +109,15 @@ const Detail = () => {
       queryClient.invalidateQueries("study");
     },
   });
-  
+
   const onDeleteBoard = async (id) => {
     const res = await deleteBoardMutate.mutateAsync(id);
   };
 
-
-  // 스터디보드에 접속한 유저 아이디뽑기 
+  // 스터디보드에 접속한 유저 아이디뽑기
   const userInfo = useRecoilValue(userInfoState);
   const res = useQuery("detailInfo", () => {
-    return getUserDetailInfo(userInfo.memberName);
+    return getUserDetailInfo(userInfo.memberId);
   });
   if (res.isLoading === false) {
     // console.log(userInfo.memberName);
@@ -110,21 +125,25 @@ const Detail = () => {
 
   // 데이터 Query로 가져오기
   const { isLoading, data } = useQuery("study", () => getStudy(id));
-  if (isLoading === false) console.log(data.data)
+  if (isLoading === false) console.log(data.data);
   const likedStatus = data?.data.wished;
   const appliedStatus = data?.data.applied;
   const approvedStatus = data?.data.approved;
-  const approvedMembers = data?.data.appliedMembers?.filter((member) => member.approved === true)
-  const boardInfos = data?.data.studyBoards.sort((a,b) => b.id - a.id)
-  
+  const approvedMembers = data?.data.appliedMembers?.filter(
+    (member) => member.approved === true
+  );
+  const boardInfos = data?.data.studyBoards.sort((a, b) => b.id - a.id);
+
   //각 방명록 정보
-  const boardData = boardInfos?.map(({ id, memberName, title, content, createdAt }) => ({
-    id,
-    memberName,
-    title,
-    content,
-    createdAt
-  }));
+  const boardData = boardInfos?.map(
+    ({ id, memberName, title, content, createdAt }) => ({
+      id,
+      memberName,
+      title,
+      content,
+      createdAt,
+    })
+  );
 
   return (
     <DetailWrapper>
@@ -194,54 +213,52 @@ const Detail = () => {
           <OneLineDesc>{data.data.content}</OneLineDesc>
 
           <StCommentText>방명록 쓰기</StCommentText>
-          <StInput 
-          type ="text"
-          placeholder = {`${userInfo.memberName}님의 방명록 제목`}
-          value = {title}
-          onChange = {titleChangeHandler}
+          <StInput
+            type="text"
+            placeholder={`${userInfo.memberName}님의 방명록 제목`}
+            value={title}
+            onChange={titleChangeHandler}
           />
           <StInput
-          height = '12rem'
-          type ="text"
-          placeholder = '내용을 입력해 주세요'
-          value = {content}
-          onChange = {contentChangeHandler}
+            height="12rem"
+            type="text"
+            placeholder="내용을 입력해 주세요"
+            value={content}
+            onChange={contentChangeHandler}
           />
-          <Button 
-          wh = "l"
-          onClick = {() => onClickPostBoard({id, board})}
-          >제출</Button>
+          <Button wh="l" onClick={() => onClickPostBoard({ id, board })}>
+            제출
+          </Button>
 
           <StCommentText>방명록 모음</StCommentText>
 
           <div>
-          {boardData && boardData.map((item, idx) => (
-          <BoardBox key={idx}>
-            <div className="memberName">{item.memberName}</div>
-            <div className="createdAt">{item.createdAt}</div>
-            <div className="boardTitle">{item.title}</div>
-            <div className="boardContent">{item.content}</div>
-            <div className="input">
-            <CommentInput type = "text"/>
-            <Button 
-            wh = 's'
-            >댓글 추가</Button>
-            </div>
-            
-            {item.memberName === userInfo.memberName &&
-            <Button 
-            wh="m" 
-            className="deleteButton"
-            onClick = {() => onDeleteBoard(item.id)}
-            >방명록 삭제</Button>
-            }
-          </BoardBox>
-           ))}
-            
-          </div>    
-          
-      </>
-    )}
+            {boardData &&
+              boardData.map((item, idx) => (
+                <BoardBox key={idx}>
+                  <div className="memberName">{item.memberName}</div>
+                  <div className="createdAt">{item.createdAt}</div>
+                  <div className="boardTitle">{item.title}</div>
+                  <div className="boardContent">{item.content}</div>
+                  <div className="input">
+                    <CommentInput type="text" />
+                    <Button wh="s">댓글 추가</Button>
+                  </div>
+
+                  {item.memberName === userInfo.memberName && (
+                    <Button
+                      wh="m"
+                      className="deleteButton"
+                      onClick={() => onDeleteBoard(item.id)}
+                    >
+                      방명록 삭제
+                    </Button>
+                  )}
+                </BoardBox>
+              ))}
+          </div>
+        </>
+      )}
     </DetailWrapper>
   );
 };
